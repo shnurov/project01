2dcb8301b997ac588b0fe5d4906edb466ca597e1
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-06-21 10:48:25 +0400

    feat: add login

--- index.js
+++ index.js
@@ -19,6 +19,51 @@ const app = express();
 
 app.use(express.json());
 
+// АВТОРИЗАЦИЯ
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден',
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+          return res.status(403).json({
+            message: 'Неверный логин или пароль',
+          });
+        }
+
+        const token = jwt.sign(
+          {
+              _id: user._id,
+          },
+          'secret123',
+          {
+              expiresIn: '30d',
+          },
+      );
+
+    const {passwordHash, ...userData} = user._doc;
+
+    res.json({
+        ...userData,
+        token,
+    });
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Не удалось авторизоваться",
+      });
+    }
+});
+
+// РЕГИСТРАЦИЯ
 app.post("/auth/register", registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);
@@ -61,6 +106,13 @@ app.post("/auth/register", registerValidation, async (req, res) => {
       message: "Не удалось зарегистрироваться",
     });
   }
+});
+
+// ЗАПРОС ИНФЫ
+app.get('/auth/me', (req, res) => {
+  try {
+
+  } catch (err) {}
 });
 
 app.listen(4444, (err) => {
